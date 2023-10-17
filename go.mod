191cb714f73801a04225118c7d47248a3fe688e2
Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>
2023-10-16 23:37:54 -0600

    linter

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/datumforge/go-template
 
-go 1.21.3
+go 1.21
 
 require (
 	github.com/99designs/gqlgen v0.17.39
--- main.go
+++ main.go
@@ -1,7 +1,4 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
+// package main is the entry point
 package main
 
 import "github.com/datumforge/go-template/cmd"
