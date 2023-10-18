6e409670cd4b78917c5aff48ba84ee903fb9db09
Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>
2023-10-18 11:21:50 -0600

    Add script to generate basic graphql shemas
    Signed-off-by: Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>

---
+++ scripts/gen_graphql.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Get all generated schema files from ent
+entSchemaDir=internal/ent/schema
+graphSchemaDir=schema
+schemas=$(find $entSchemaDir -name '*.go')
+
+for file in $schemas
+do
+    file=${file##*/}
+    schema=${file%.*}
+    # Check if file already exists
+    if [ -f "$graphSchemaDir/$schema.graphql" ]
+    then
+        echo "$graphSchemaDir/$schema.graphql already exists, not regenerating."
+    else
+
+        touch $graphSchemaDir/$schema.graphql
+
+        export name="${schema}"
+
+        # Object is capitilized
+        first=`echo $name|cut -c1|tr [a-z] [A-Z]`
+        second=`echo $name|cut -c2-`
+        export object="${first}${second}"
+
+        # Generate a base graphql schema
+        gomplate -f scripts/graph.tpl > $graphSchemaDir/$schema.graphql
+
+        echo +++ file created $graphSchemaDir/$schema.graphql
+    fi
+done
